78327f48e01ffc6d641743a4252ed827c8ef4be3
Eric Preston <epreston137@gmail.com>
2025-04-18 22:28:48 +0000

    remove cents from SAFE boxes in valuation cap, investment amount and company valuation at conversion

--- src/components/SafeConverter.tsx
+++ src/components/SafeConverter.tsx
@@ -53,6 +53,8 @@ const SafeConverter = () => {
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
     }).format(value);
   };
 
