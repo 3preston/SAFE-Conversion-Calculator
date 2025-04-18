d1db8cf953eb6edcea2a2fea403f63abd648119c
Eric Preston <epreston137@gmail.com>
2025-04-18 22:48:46 +0000

    change heading of SAFE COnverter to be SAFE Conversion Calculactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,11 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-4xl font-bold">
-          SAFE Converter
+          SAFE Conversion Calculator
         </h1>
         <SafeConverter />
       </main>
     </div>
   );
 }
+
